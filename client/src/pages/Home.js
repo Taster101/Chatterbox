80797c8406f5cbc1302d5608d1a4b56af8cc4558
Taster101 <Davontahenderson1@gmail.com>
2023-06-16 09:37:34 -0500

    created query function for tag search

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,14 @@ import { useQuery } from '@apollo/client';
 import PostFormCard from "../components/PostFormCard";
 import PostCard from "../components/PostCard";
 import Layout from "../components/Layout";
-import { ALL_MEMORIES  } from '../utils/queries';
+import { ALL_MEMORIES , TAG_SEARCH } from '../utils/queries';
+
 
 // import Login from "./Login";
 
 const Home = () => {
   const { loading, data } = useQuery(ALL_MEMORIES);
+  const {loader, datas } = useQuery(TAG_SEARCH);
   const memories = data?.memory || [];
   // const session = useSession();
   return (
