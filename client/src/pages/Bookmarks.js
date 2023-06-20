913ff338f543b39b9afabfd0b17cce229d41bd48
Tim Dolan <timmydolan14@gmail.com>
2023-06-19 20:44:04 -0400

    Updated Bookmarks page

--- client/src/pages/Bookmarks.js
+++ client/src/pages/Bookmarks.js
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout";
 import PostCard from "../components/PostCard";
 
-export default function Bookemarks() {
+export default function Bookmarks() {
     return(
         <Layout>
             <h1 className="text-6xl mb-4 text-gray-300"> Saved posts</h1>
